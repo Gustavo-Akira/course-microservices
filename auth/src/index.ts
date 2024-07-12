c7de497b252547be6e3859655224aaa54d97b8b3
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-07-12 17:52:00 -0300

    feat: jwt creation and jwt secret on kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -28,6 +28,9 @@ app.get('*',async (req, res)=>{
 app.use(errorHandler);
 
 const start = async () =>{
+  if(!process.env.JWT_KEY){
+    throw new Error('JWT key must be defined');
+  }
   await mongoose.connect("mongodb://auth-mongo-srv:27017");
   app.listen(3000, () => {
     console.log('Listening on port 3000!');
