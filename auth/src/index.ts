109ecab9f4a4bd86454ef25b275c337be9d12034
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-07-12 16:36:10 -0300

    setup: addinng cookiesession and trust proxy for nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -11,6 +12,11 @@ import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
+app.set('trust proxy',true);
+app.use(cookieSession({
+  signed:false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
