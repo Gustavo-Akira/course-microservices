03c0d6c7fd58c436910ad1f7044ff6dffc38187e
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-07-14 12:21:59 -0300

    refact/setup: app to separate for tests and jest mongodb memory server

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,15 @@
-import express from 'express';
-import { json } from 'body-parser';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.use(json());
-app.set('trust proxy',true);
-app.use(cookieSession({
-  signed:false,
-  secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.get('*',async (req, res)=>{
-   throw new NotFoundError();
-});
-app.use(errorHandler);
-
+import { DatabaseConnectionError } from './errors/database-connection-error';
+import {app} from './app';
 const start = async () =>{
   if(!process.env.JWT_KEY){
     throw new Error('JWT key must be defined');
   }
-  await mongoose.connect("mongodb://auth-mongo-srv:27017");
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017");
+  }catch(err){
+    throw new DatabaseConnectionError();
+  }
   app.listen(3000, () => {
     console.log('Listening on port 3000!');
   });
