19fb255ce3bae9149417e7df6413688caffed01e
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-07-15 16:52:46 -0300

    feat: user signin page and image and setup of kubernetes(ingress, yaml, skaffold)

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+import axios from 'axios';
+import useRequest from '../../hooks/use-request';
+export default ()=>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword]= useState("");
+    const {doRequest, errors} = useRequest({url:"/api/users/signup",method:"post",body:{email, password}});
+    const onSubmit= async (e)=>{
+        e.preventDefault();
+        doRequest();
+    }
+    return <form onSubmit={onSubmit}>
+        <h1>Cadastro</h1>
+        <div className="form-group">
+            <label>Email</label>
+            <input value={email} onChange={e => setEmail(e.target.value)} className="form-control"/>
+        </div>
+        <div className="form-group">
+            <label>Senha</label>
+            <input value={password} onChange={e=>setPassword(e.target.value)} className="form-control"/>
+        </div>
+        {errors}
+        <button className="btn btn-primary">Cadastrar</button>
+    </form>
+};
