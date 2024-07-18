18d83ef52d2d1e7bb6a1edd6e21f34b6ea30791d
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-07-18 20:34:14 -0300

    test: adding validation test for ticket

--- ticket/src/routes/__test__/new.test.ts
+++ ticket/src/routes/__test__/new.test.ts
@@ -19,11 +19,42 @@ it('returns a status other than 401 signed in', async()=>{
 });
 
 it('returns an error if an invalid title is provided', async()=>{
-    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10.0
+        }).expect(400);
+        await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10.0
+        }).expect(400);
 });
 
 it('returns an error if an invalid price is provided', async()=>{
-    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'test',
+            price: 0
+        }).expect(400);
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'test',
+            price: -10
+        }).expect(400);
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'test'
+        }).expect(400);
 });
 
 it('creates a ticket with valid inputs', async()=>{
